f7ac11a6cdb0b35621d291ae82533ccd0d9f71ff
Michael Espinosa <maespinosa9@misena.edu.co>
2015-04-08 17:01:59 -0500

    Adiciones Modelo Profesional

--- controlador/cProfesional.php
+++ controlador/cProfesional.php
@@ -2,6 +2,15 @@
 include("modelo/mProfesional.php");
 include ("modelo/mPagina.php");	
 
+$ins = new mProfesional();
+
+$delete = isset($_GET["del"]) ? $_GET["del"]:NULL;
+
+$filtro=isset($_GET["filtro"]) ? $_GET["filtro"]:NULL;
+$pac = isset ($_GET["pac"]) ? $_GET["pac"]:NULL;
+$pr = isset($_GET['pr']) ? $_GET['pr']:NULL;
+$actu = isset($_POST["actu"]) ? $_POST["actu"]:NULL;
+
 
 
 ?>
--- modelo/mProfesional.php
+++ modelo/mProfesional.php
@@ -20,7 +20,45 @@ class mProfesional{
 		$sql .= "Direccion, Telefono, Celular, E_mail, Observaciones, Conmutador) VALUES '".$IdUsuario."', '".$IdTipoIdentificacion."', '".$Identificacion."', '".$PriApe."', ";
 		$sql .= "'".$SegApe."', '".$PriNom."', '".$SegNom."', '".$RegistroProf."', '".$Direccion."', '".$Tel."', '".$Cel."', '".$mail."', '".$observ."', '".$Conmutador."'";
 		$this->cons($sql);
-	}	
+	}
+
+	function UpdProf($IdProfesional, $IdUsuario, $IdTipoIdentificacion, $Identificacion, $PriApe, $SegApe, $PriNom, $SegNom, $RegistroProf, $Direccion, $Tel, $Cel, $mail, $observ, $Conmutador)
+	{
+		$sql = "UPDATE profesional SET IdUsuario='".$IdUsuario."',IdTipoIdentificacion='".$IdTipoIdentificacion."' ";
+		$sql .= ",Identificacion='".$Identificacion."',PrimerApellido='".$PriApe."',SegundoApellido='".$SegApe."' ";
+		$sql .= ",PrimerNombre='".$PriNom."',SegundoNombre='".$SegNom."',RegistroProfesional='".$RegistroProf."' ";
+		$sql .= ",Direccion='".$Direccion."',Telefono='".$Tel."',Celular='".$Cel."',E_mail='".$mail."' ";
+		$sql .= ",Observaciones='".$observ."',Conmutador='".$Conmutador."' WHERE IdProfesional = '".$IdProfesional."'";
+		$this->cons($sql);
+	}
+
+	function SelProf($filtro,$rvalini,$rvalfin)
+	{
+		$sql = "SELECT * from Profesional";
+		if($filtro)
+			$sql.= " WHERE Profesional.Identificacion LIKE '%".$filtro."%'";
+		$sql.= " ORDER BY Profesional.IdProfesional LIMIT ".$rvalini.", ".$rvalfin;
+		$conexionBD = new conexion();
+		$conexionBD->conectarBD();
+		$data = $conexionBD->ejeCon($sql,0);
+		return $data;
+	}
+
+	function SelUsu(){
+		$sql = "SELECT * from Usuario";
+		$conexionBD = new conexion();
+		$conexionBD->conectarBD();
+		$data = $conexionBD->ejeCon($sql,0);
+		return $data;
+	}
+
+	function SelTipoId(){
+		$sql = " SELECT * FROM TipoIdentificacion";
+		$conexionBD = new conexion();
+		$conexionBD->conectarBD();
+		$data = $conexionBD->ejeCon($sql,0);
+		return $data;
+	}
 
 }
 
