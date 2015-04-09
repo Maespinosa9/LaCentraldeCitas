7409f0dca421d0ce1f652bd3473648e786fab2dd
Michael Espinosa <maespinosa9@misena.edu.co>
2015-04-09 18:53:41 -0500

    VProf

--- vista/vProfesional.php
+++ vista/vProfesional.php
@@ -1,3 +1,11 @@
 <?php
 include("controlador/cProfesional.php")
 ?>
+
+<div align="left">
+
+<h3>Ingresar Profesionales</h3>
+
+<div class="form-group">
+	<form id="formProf" name = "formProf" method="POST" action="" class="form-horizontal">
+		
